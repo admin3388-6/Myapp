4a1d3697d3ed6df170dd2cbdfc8a7ca0848bbb2a
Sky data <lolipop1999x@gmail.com>
2026-04-22 21:43:18 +0100

    Create MainActivity.java

---
+++ app/src/main/java/com/skydata/app/MainActivity.java
@@ -0,0 +1,39 @@
+package com.skydata.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+import com.onesignal.OneSignal;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String ONESIGNAL_APP_ID = "48370ee9-5129-4aa7-994a-d3dd8368c5f7";
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
+
+        myWebView = findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://skydata.bond");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
